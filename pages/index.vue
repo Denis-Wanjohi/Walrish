acf9128b69b1863e8ba68427bd48424eec5f9242
Denis-Wanjohi <deniswanjohi15@gmail.com>
2025-06-02 20:31:17 +0300

    moved  the content from app.vue to the pages index page

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="bg-black hide-scrollbar">
+        <Hero2 id=""/>
+    <Services id="services" class="pt-10"/>
+    <About id="about_us" class="pt-10"/>
+    <Statistics/>
+    <Projects class="bg-black"/>
+    <Testimonials/>
+    <ContactForm/>
+    <Footer id="contacts"/>
+    </div>
+</template> 
