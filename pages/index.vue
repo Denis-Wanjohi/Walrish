b786a30360d00fd4b15f544f4ad589d546ca1c24
Denis-Wanjohi <deniswanjohi15@gmail.com>
2025-06-23 17:54:03 +0300

    managed route back to the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="bg-black hide-scrollbar">
         <Hero2 id=""/>
-    <Services id="services" class="pt-10"/>
-    <About id="about_us" class="pt-10"/>
+    <Services id="services" class="pt-20"/>
+    <About id="about_us" class="pt-20"/>
     <Statistics/>
-    <Projects class="bg-black"/>
-    <Testimonials/>
-    <ContactForm/>
-    <Footer id="contacts"/>
+    <Projects id="projects" class="bg-black"/>
+    <Testimonials id="testimonials"/>
+    <ContactForm id="contacts" class="pt-20"/>
+    <Footer id="footer"/>
     </div>
 </template> 
+
+<script setup>
+definePageMeta({ssr:false})
+onMounted(()=>{
+    if(useRoute().fullPath.includes("section")){
+        let section = useRoute().query.section
+        useRouter().push('/#'+section)
+    }
+})
+</script>
